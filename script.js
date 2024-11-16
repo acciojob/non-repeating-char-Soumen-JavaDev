f257cd4fcc1f9bce7365a33c3a471a1c8d0dbadc
SOUMEN MONDAL <151416831+Soumen-JavaDev@users.noreply.github.com>
2024-11-16 10:06:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+  const map=new Map();
+
+	for(let char of str){
+		map.set(char,(map.get(char) ||0)+1);
+	}
+	for(let i of str){
+		if(map.get(i)===1){
+			return i;
+		}
+	}
+	return null;
+	
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
